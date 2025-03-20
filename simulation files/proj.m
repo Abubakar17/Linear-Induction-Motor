bb4827e64f2ea812bf09a1803f07ac64dc8f9387
Abubakar <89094550+Abubakar17@users.noreply.github.com>
2025-03-20 12:02:31 +0500

    Add files via upload

---
+++ simulation files/proj.m
@@ -0,0 +1,13 @@
+
+Rr = 10.166; %values according to lab volt
+Rs = 1.6875;
+Llr = 0.0323;
+Lm = 0.0420;
+Lls = 0.0788;
+P = 2;  %pole pairs
+M = 16.1; %mass of the mover
+Ws = 314.5; %frequency rad/s
+D = 2.150;  % primary length 2150mm length of stator
+Vo = 220;
+t = 0.358; %pole pitch  
+B = 0.93; %coefficient of friction
